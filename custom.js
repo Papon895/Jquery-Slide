ba890ef8456bb9c07acdf0cc8321b9bdaad77847
Papon Barmon <78915190+Papon895@users.noreply.github.com>
2021-10-22 15:14:09 +0600

    Jquery Slide
    Jquery slideup,slidedown and slidetoggle from web ship.

---
+++ custom.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+  $('.first').click(function(){
+    $('.about').slideToggle('3000');
+  });
+  $('.secound').click(function(){
+    $('.contact').slideToggle('3000');
+  });
+  $('.third').click(function(){
+    $('.work').slideToggle('3000');
+  });
+});
